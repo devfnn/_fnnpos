e878087cfbb4842741329c9e77d6b4abaf00d7c0
suwanim <suwan.im@gmail.com>
2022-03-29 16:07:23 +0700

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-var app = require('../app');
-var http = require('http');
-
-const port = normalizePort(process.env.PORT || '3002');
-app.set('port', port);
-const server = http.createServer(app);
-server.listen(port);
